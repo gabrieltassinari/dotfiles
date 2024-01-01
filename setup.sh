5b3156f49b0f5e235d20ea69bc9973f9aa5b2795
gabrieltassinari <gabrieldtassinari@gmail.com>
2024-01-01 20:49:01 -0300

    Update: modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Create symbolic links for all files
-for f in .*
-do
-	if [[ -f $f ]]; then
+# Symbolic links for . files
+for f in .*; do
+	if [[ $f != ".git" ]]; then
 		ln -sf $HOME/dotfiles/$f $HOME/$f
 		echo "symbolic link created: $f"
 	fi
 done
 
-mkdir $HOME/.vim 2>/dev/null
-ln -s $HOME/dotfiles/.vim/coc-settings.json $HOME/.vim/coc-settings.json
+# Symbolic links for .config/ files
+[[ -z $HOME/.config ]] && mkdir $HOME/.config
 
-# Move .config/ directory
-if [[ -d $HOME/.config ]]; then
-	mv .config/* $HOME/.config
-else
-	mv .config $HOME
-fi
+for d in .config/*; do ln -s $HOME/dotfiles/$d $HOME/$d; done
