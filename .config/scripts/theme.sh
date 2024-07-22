460ea61273a4390ebf5c478fa7a6b6ab77c7848c
gabrieltassinari <100461539+gabrieltassinari@users.noreply.github.com>
2024-07-22 19:49:16 -0300

    alacritty: add yaml file

--- .config/scripts/theme.sh
+++ .config/scripts/theme.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-alacritty=$HOME/.config/alacritty/alacritty.toml
+# Newer alacritty versions only supports yml files.
+alacritty_toml=$HOME/.config/alacritty/alacritty.toml
+alacritty_yaml=$HOME/.config/alacritty/alacritty.yml
+
 nvim=$HOME/.config/nvim/init.lua
 
 if grep -q "'light'" $nvim; then
-    sed -Ei "s/light/dark/g" $alacritty
+    sed -Ei "s/light/dark/g" $alacritty_toml $alacritty_yaml
     sed -Ei "s/'light'/'dark'/g" $nvim
 else
-    sed -Ei "s/dark/light/g" $alacritty
+    sed -Ei "s/dark/light/g" $alacritty_toml $alacritty_yaml
     sed -Ei "s/'dark'/'light'/g" $nvim
 fi
