bc4d6595eb28cc53334c186b94b6886c068cac86
Gabriel Tassinari <gabrieldtassinari@gmail.com>
2025-08-14 15:23:13 -0300

    nvim: use only double quotes and add docs mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,12 +8,12 @@ vim.pack.add({
   {src = "https://github.com/hrsh7th/cmp-nvim-lsp"},
   {src = "https://github.com/hrsh7th/cmp-buffer"},
   {src = "https://github.com/hrsh7th/nvim-cmp"},
-  {src = "https://github.com/L3MON4D3/LuaSnip"}
+  {src = "https://github.com/L3M0N4D3/LuaSnip"},
 })
 
 -- nvim-treesitter
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "html", },
+require("nvim-treesitter.configs").setup {
+  ensure_installed = { "c", "lua", "vim", "html", "vimdoc" },
   highlight = { enable = true },
   indent = { enable = true },
 }
@@ -21,13 +21,13 @@ require('nvim-treesitter.configs').setup {
 -- nvim-lspconfig
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(e)
-    local telescope = require('telescope.builtin')
+    local telescope = require("telescope.builtin")
     local opts = { buffer = e.buf }
 
-    vim.keymap.set("n", 'gI', telescope.lsp_implementations, opts)
-    vim.keymap.set("n", 'gd', telescope.lsp_definitions, opts)
-    vim.keymap.set("n", 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gI", telescope.lsp_implementations, opts)
+    vim.keymap.set("n", "gd", telescope.lsp_definitions, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   end,
 })
 
@@ -38,24 +38,30 @@ vim.lsp.enable({
 })
 
 -- nvim-cmp
-require('cmp').setup({
+local cmp = require("cmp")
+cmp.setup({
   snippet = {
     expand = function(args)
       vim.snippet.expand(args.body)
     end,
   },
 
-  sources = require('cmp').config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-5),
+    ["<C-f>"] = cmp.mapping.scroll_docs(5),
+  }),
+
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
   }, {
-    { name = 'buffer' }
+    { name = "buffer" }
   })
 })
 
 -- General config
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = false
@@ -63,20 +69,20 @@ vim.api.nvim_set_option("clipboard","unnamed")
 
 -- Mapping
 local colorcolumn = function()
-  if (vim.o.cc == '80') then vim.o.cc = '0' else vim.o.cc = '80' end
+  if (vim.o.cc == "80") then vim.o.cc = "0" else vim.o.cc = "80" end
 end
 
 local disable_diagnostic = function()
   vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end
 
-vim.keymap.set("n", 'c,', colorcolumn)
-vim.keymap.set("n", 'c.', disable_diagnostic)
-vim.keymap.set("n", 'g?', vim.diagnostic.open_float)
+vim.keymap.set("n", "c,", colorcolumn)
+vim.keymap.set("n", "c.", disable_diagnostic)
+vim.keymap.set("n", "g?", vim.diagnostic.open_float)
 
 -- Replace increment key due to tmux bind C-a
-vim.keymap.set({'n', 'v'}, '<C-b>', '<C-a>')
-vim.keymap.set('v', 'g<C-b>', 'g<C-a>')
+vim.keymap.set({"n", "v"}, "<C-b>", "<C-a>")
+vim.keymap.set("v", "g<C-b>", "g<C-a>")
 
 -- telescope.nvim
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files)
@@ -88,8 +94,8 @@ vim.opt.undofile = false
 vim.opt.swapfile = false
 
 -- Colorscheme
-vim.cmd.colorscheme('sunbather')
-vim.opt.background = 'dark'
+vim.cmd.colorscheme("sunbather")
+vim.opt.background = "dark"
 vim.opt.termguicolors = false
 
 -- Transparency
